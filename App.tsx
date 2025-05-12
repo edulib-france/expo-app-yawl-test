f21c1a2efbddb66c8e31fa34b805381ba87e44bb
Felix Salazar <fxsalazar@gmail.com>
2025-05-12 14:21:12 +0200

    refactor: enhance navigation setup and improve HomeScreen functionality

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  NavigationContainerRef,
+} from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Text, View } from "react-native";
+import { useRef } from "react";
+import { Button, Text, View } from "react-native";
 
-function HomeScreen() {
+function HomeScreen({ navigation }: { navigation: any }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+}
+function DetailScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
     </View>
   );
 }
@@ -13,13 +27,32 @@ function HomeScreen() {
 const Stack = createStackNavigator();
 
 function App() {
+  const navigationRef = useRef<NavigationContainerRef>(null);
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      ref={navigationRef}
+      onStateChange={async () => {
+        const rt = navigationRef.current?.getCurrentRoute();
+        console.log("🚀 ===> ~ onStateChange:", rt);
+      }}
+    >
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 export default App;
+
+const YawlNavigationContainer = () => {
+  return (
+    <NavigationContainer>
+      <h1></h1>
+      {/* <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator> */}
+    </NavigationContainer>
+  );
+};
